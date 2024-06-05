0d534734cfbdca9c9c7885bbaafc0434b92ac10e
Mazen-4 <mazenmoamen02@gmail.com>
2024-06-05 18:22:37 +0300

    Add localization support for English and Arabic languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:al_asas/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       title: 'Al Asas',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2A3E34)),
